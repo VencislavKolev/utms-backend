7207cb88dfda761bfd6e747556830eeb720390b9
VencislavKolev <venci362@gmail.com>
2021-05-13 19:24:49 +0300

    update ip address

--- src/main/java/com/vmware/utms/service/HttpClientService.java
+++ src/main/java/com/vmware/utms/service/HttpClientService.java
@@ -12,8 +12,8 @@ import java.net.http.HttpResponse;
 public class HttpClientService {
 //    private static final String PROJECT_ENDPOINT = "http://localhost:8081/api/projects";
 //    private static final String TESTRUN_ENDPOINT = "http://localhost:8081/api/projects/%s/runs";
-    private static final String PROJECT_ENDPOINT = "http://192.168.100.1:8081/api/projects";
-    private static final String TESTRUN_ENDPOINT = "http://192.168.100.1:8081/api/projects/%s/runs";
+    private static final String PROJECT_ENDPOINT = "http://192.168.56.1:8081/api/projects";
+    private static final String TESTRUN_ENDPOINT = "http://192.168.56.1:8081/api/projects/%s/runs";
 
     public String sendRequest(String json) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
